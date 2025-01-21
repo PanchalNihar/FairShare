51a0cbbda7d3ca1ffa016a134d6fc32afd9210fe
PanchalNihar <nihar2624@gmail.com>
2025-01-21 13:39:47 +0530

    new changes

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,18 +1,24 @@
-import { inject } from '@angular/core';
+import { inject, PLATFORM_ID } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from './services/auth.service';
-
+import { isPlatformBrowser } from '@angular/common';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
-  const authService = inject(AuthService); // Inject AuthService
+  const authService = inject(AuthService);
+  const platformId = inject(PLATFORM_ID); // Inject PLATFORM_ID
 
-  const isLoggedIn = authService.isAuthenticated();
-  
-  if (!isLoggedIn) {
-    router.navigate(['/signin']);
-    return false;
+  // Check if the code is running in the browser
+  if (isPlatformBrowser(platformId)) {
+    const isLoggedIn = authService.isAuthenticated();
+
+    if (!isLoggedIn) {
+      router.navigate(['/signin']);
+      return false;
+    }
+    return true;
   }
 
-  return true;
+  // If on the server, block navigation
+  return false;
 };
